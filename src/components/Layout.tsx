6fce451aba100bafadb27380e7acad24df19531c
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2023-04-05 00:38:50 +0900

    :airplane: ヒーローイメージを追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,7 @@
 import { Klee_One } from "next/font/google"
+import Image from "next/image"
+
+import variables from "@/styles/variables.module.scss"
 
 import styles from "./Layout.module.scss"
 
@@ -14,7 +17,28 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className={`${primaryFont.variable} ${styles.layout}`}>{children}</div>
+    <div className={`${primaryFont.variable} ${styles.layout}`}>
+      <div className={styles.hero}>
+        <Image
+          className={styles.illust}
+          src="/images/illust.png"
+          alt="yuarasino illust"
+          sizes={`${variables.middleQuery} 50vw, 100vw`}
+          fill
+          priority
+        />
+        <Image
+          className={styles.logo}
+          src="/images/logo.png"
+          alt="yuarasino logo"
+          sizes={`${variables.middleQuery} 25vw, 50vw`}
+          width={569}
+          height={344}
+          priority
+        />
+      </div>
+      <div className={styles.page}>{children}</div>
+    </div>
   )
 }
 
