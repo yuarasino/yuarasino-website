07bc87a647239e3cb14bc799726afcb685ea7dcb
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-04-21 05:42:09 +0900

    :rocket: TailwindCSSの設定を追加

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,12 @@ export default {
   semi: false,
   singleQuote: false,
   jsxSingleQuote: false,
-  plugins: ["prettier-plugin-astro", "prettier-plugin-astro-organize-imports"],
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-astro-organize-imports",
+    "prettier-plugin-organize-imports",
+  ],
   overrides: [
     {
       files: "*.astro",
