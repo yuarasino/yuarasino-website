73f8a20e131a58a5657630685ac2949d74ca82ca
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-06-12 02:41:34 +0900

    🎨 フォント設定を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,10 @@ export default {
   content: [
     "src/{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    fontFamily: {
+      sans: ["'Inter'", "'Noto Sans JP'", "sans-serif"],
+      mono: ["'Noto Sans Mono'", "'Noto Sans JP'", "monospace"],
+    },
+  },
 } satisfies Config;
