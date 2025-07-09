159743cd98ad4459077184a0a1ff7b03d6b64ed0
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-07-10 00:42:31 +0900

    💄 フォントを追加

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -8,6 +8,20 @@ export default define.page(function App({ Component, state }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{state.title}</title>
         <meta name="description" content={state.description} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400..700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400..700&display=swap"
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,9 @@ export default {
   content: [
     "src/{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    fontFamily: {
+      sans: ["'Noto Sans'", "'Noto Sans JP'", "sans-serif"],
+    },
+  },
 } satisfies Config;
