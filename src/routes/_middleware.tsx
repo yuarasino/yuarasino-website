bed4935bee50de9e59c34a803545941f04f5bb0f
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-07-20 02:28:32 +0900

    ⚡️ アクセスロガーのURLをパスに変更

--- src/routes/_middleware.tsx
+++ src/routes/_middleware.tsx
@@ -2,7 +2,7 @@ import { define } from "../utils/fresh.ts";
 
 const accessLogger = define.middleware(async (ctx) => {
   const res = await ctx.next();
-  console.log(`${ctx.req.method} ${ctx.req.url} ${res.status}`);
+  console.log(`${ctx.req.method} ${ctx.url.pathname} ${res.status}`);
   return res;
 });
 
