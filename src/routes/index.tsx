e8ba47b962d118a58cc7b0949d8226e49b0105bb
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-09 01:30:38 +0900

    💄 レスポンシブコンテナを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { page } from "fresh";
 import { define } from "../utils/fresh.ts";
 import { SvgIcon } from "../components/SvgIcon.tsx";
+import { ResponsiveContainer } from "../components/ResponsiveContainer.tsx";
 
 export const handler = define.handlers({
   GET: ({ state }) => {
@@ -13,14 +14,18 @@ export const handler = define.handlers({
 export default define.page(({ state }) => {
   return (
     <main class="min-h-screen">
-      <h1 class="text-pink-500">{state.title}</h1>
-      <p class="text-blue-500">{state.description}</p>
-      <p>
-        <SvgIcon
-          src="/icons/chevron-double-down.svg"
-          alt="コンテンツに移動する"
-        />
-      </p>
+      <section>
+        <ResponsiveContainer class="py-8">
+          <h1 class="text-pink-500">{state.title}</h1>
+          <p class="text-blue-500">{state.description}</p>
+          <p>
+            <SvgIcon
+              src="/icons/chevron-double-down.svg"
+              alt="コンテンツに移動する"
+            />
+          </p>
+        </ResponsiveContainer>
+      </section>
     </main>
   );
 });
