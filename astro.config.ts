8633ecc9ddb40d35faf7ed0334dfdfcded7583ed
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-17 05:05:36 +0900

    🔨 Tailwindを導入

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import deno from "@deno/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
     plugins: [
       deno(),
+      tailwindcss(),
     ],
   },
 });
