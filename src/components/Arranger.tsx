3a81eadf532644fe9575803c86b8a0dad84086f5
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-18 11:13:30 +0900

    🎨 アレンジャを追加

---
+++ src/components/Arranger.tsx
@@ -0,0 +1,16 @@
+import { defineComponent } from "@/utils/preact.ts";
+import { cn } from "@/utils/tailwind.ts";
+
+export type ArrangerProps = {
+  class?: string;
+};
+
+export const Arranger = defineComponent<ArrangerProps>((
+  { class: class_, children },
+) => {
+  return (
+    <div class={cn(class_)}>
+      {children}
+    </div>
+  );
+});
