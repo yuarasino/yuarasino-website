558b350e5935bc1cf0a93ca1385d5e625b304b35
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-18 10:44:07 +0900

    🔨 GoogleFontsを導入

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -8,6 +8,27 @@ export default define.page(({ Component, state }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{state.title}</title>
         <meta name="description" content={state.description} />
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap"
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
