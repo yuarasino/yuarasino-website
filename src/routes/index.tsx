0c356ce753a9e47c412a445845e2dca10fedbf10
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-18 09:44:26 +0900

    🔨 TailwindCSSを導入

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { page } from "fresh";
 import { define } from "@/utils/fresh.ts";
+import { cn } from "@/utils/tailwind.ts";
 
 export const handler = define.handlers({
   GET: ({ state }) => {
@@ -13,8 +14,8 @@ export default define.page<typeof handler>(({ state }) => {
   return (
     <main>
       <section>
-        <h1 class="text-pink-500">{state.title}</h1>
-        <p class="text-blue-500">{state.description}</p>
+        <h1 class={cn("text-pink-500")}>{state.title}</h1>
+        <p class={cn("text-blue-500")}>{state.description}</p>
       </section>
     </main>
   );
