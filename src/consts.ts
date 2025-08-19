90483bd37091e854c367a3c2598d03c2411b121b
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-19 12:17:58 +0900

    🎨 ヘッダーを追加

---
+++ src/consts.ts
@@ -0,0 +1,30 @@
+import type { NavLink } from "./types.ts";
+
+export const siteName = "YUARASINO.NET";
+
+export const navLinks: NavLink[] = [
+  {
+    name: "PROFILE",
+    href: "/#profile",
+  },
+  {
+    name: "ACTIVITIES",
+    href: "/#activities",
+  },
+  {
+    name: "RESUME",
+    href: "/#resume",
+  },
+  {
+    name: "WORKS",
+    href: "/#works",
+  },
+  {
+    name: "BLOG",
+    href: "/blog",
+  },
+  {
+    name: "INFO",
+    href: "/info",
+  },
+];
