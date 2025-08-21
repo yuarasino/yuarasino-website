6f311325323a68bbc63fbc1f5a8e92dba7c8b429
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-21 09:43:21 +0900

    🔨 TailwindCSSを導入

--- src/dev.ts
+++ src/dev.ts
@@ -1,10 +1,12 @@
 import * as path from "@std/path";
 import { Builder } from "fresh/dev";
+import { tailwind } from "@fresh/plugin-tailwind";
 
 const builder = new Builder({
   root: path.join(Deno.cwd(), "src"),
   outDir: path.join(Deno.cwd(), "_fresh"),
 });
+tailwind(builder);
 
 if (Deno.args.includes("build")) {
   await builder.build();
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { page } from "fresh";
 import { define } from "~/utils/fresh.ts";
+import { cn } from "~/utils/tailwind.ts";
 
 export const handler = define.handlers(({ state }) => {
   state.title = "yuarasino-website";
@@ -10,7 +11,7 @@ export default define.page<typeof handler>(({ state }) => {
   return (
     <main>
       <section>
-        <h1>{state.title}</h1>
+        <h1 class={cn("text-pink-500")}>{state.title}</h1>
       </section>
     </main>
   );
