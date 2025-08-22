3ddc01377da92482265f2d666d42797f1d2fb6ed
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-23 02:50:06 +0900

    🔨 Tailwindを導入

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -7,7 +7,6 @@ export default define.page(({ Component, state }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{state.title}</title>
-        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   root: "src",
   plugins: [
     fresh(),
+    tailwind(),
   ],
 });
