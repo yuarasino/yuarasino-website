5dc4d0e6bc100cb71460e5c0ad4646b51f0c99c7
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-08-27 08:28:19 +0900

    🎨 アイコンを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Head } from "fresh/runtime";
 import { define } from "~/utils/typing.ts";
 import { cn } from "~/utils/styling.ts";
+import { Icon } from "~/components/Icon.tsx";
 
 export default define.page(() => {
   return (
@@ -11,6 +12,12 @@ export default define.page(() => {
       <main>
         <section>
           <h1 class={cn("text-pink-500")}>yuarasino-website</h1>
+          <p class={cn("text-slate-500")}>
+            <Icon
+              icon="icon-[material-symbols-light--keyboard-double-arrow-down]"
+              label="コンテンツに進む"
+            />
+          </p>
         </section>
       </main>
     </>
