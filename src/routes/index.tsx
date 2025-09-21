ab268b76e4571664327e86b65925b0201aa5a606
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-09-21 18:11:32 +0900

    💄 アイコンを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Head } from "fresh/runtime";
+import { Icon } from "~/components/Icon.tsx";
 import { define } from "~/utils/typing.ts";
 import { cn } from "~/utils/styling.ts";
 
@@ -11,6 +12,12 @@ export default define.page(() => {
       <main>
         <section>
           <h1 class={cn("text-pink-500")}>yuarasino-website</h1>
+          <p class={cn("text-slate-500")}>
+            <Icon
+              icon="icon-[material-symbols-light--keyboard-double-arrow-down]"
+              label="コンテンツに進む"
+            />
+          </p>
         </section>
       </main>
     </>
--- src/utils/typing.ts
+++ src/utils/typing.ts
@@ -1,3 +1,11 @@
 import { createDefine } from "fresh";
 
+import type { AnyComponent } from "preact";
+
 export const define = createDefine();
+
+export const defineComponent = <Props = unknown>(
+  component: AnyComponent<Props>,
+): AnyComponent<Props> => {
+  return component;
+};
